6041525b64c92497b59a6af26fafcad5ca351f88
vagrant <vagrant@vagrant.vm>
2019-02-27 05:57:56 +0000

    Flask Works

---
+++ addingProducts.py
@@ -0,0 +1,51 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Users, Products, ShoppingCart, Base
+
+engine = create_engine('sqlite:///demostore.db')
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+
+
+#----------Store items----------------
+
+# Polo_Shirt
+
+storeProduct1 = Products(name="Polo Shirt1", id = "PS1", description = "Some polo type shirt", 
+                     price = 10, size = "M", brand = "Nautica", color = "Navy" )
+
+session.add(storeProduct1)
+session.commit()
+
+storeProduct2 = Products(name="Polo Shirt2", id = "PS2", description = "Some polo type shirt", 
+                     price = 12, size = "L", brand = "Nike", color = "White" )
+
+session.add(storeProduct2)
+session.commit()
+
+
+
+
+
+#-------------------Users Registration--------------------------
+user1 = Users(name="Edgar Martinez", id = "monolet@hotmail.com", password = "monolet", carrito_total = 0)
+
+session.add(user1)
+session.commit()
+
+user2 = Users(name="Gershot", id="whatever", password="gershot", carrito_total=0)
+session.add(user2)
+session.commit()
+
